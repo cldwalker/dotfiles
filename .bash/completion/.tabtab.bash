fdb167d5b31d2337709126904026f8cdca38a547
Gabriel Horner <gabriel.horner@gmail.com>
2009-01-08 16:38:01 -0500

    added tabtab generated completions

---
+++ .bash/completion/.tabtab.bash
@@ -0,0 +1,8 @@
+complete -o default -C 'tabtab --external' curl
+complete -o default -C 'tabtab --file /Users/bozo/.tabtab_definitions/gem.rb' gem
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/cucumber.rb' cucumber
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/github.rb' github
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/github.rb --alias github' gh
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/newgem.rb' newgem
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/rails.rb' rails
+complete -o default -C 'tabtab --gem tabtab/lib/tabtab_definitions/rubyforge.rb' rubyforge
