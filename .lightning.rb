c590a230076dd8cf38fdce232e925f122d06ec39
Gabriel Horner <gabriel.horner@gmail.com>
2010-03-26 04:17:58 -0400

    tweaks

--- .lightning.rb
+++ .lightning.rb
@@ -3,5 +3,13 @@ module Lightning
     def bin
       {:paths=>ENV['PATH'].split(":").uniq.map {|e| "#{e}/*" } }
     end
+
+    def ruby_load_path
+      {:paths=>$LOAD_PATH.select {|e| e[/^\//]}.uniq.map {|e| "#{e}/**/*.rb" } }
+    end
+
+    def pics_dir
+      {:paths=>["/Users/bozo/misc/pics/**/"]}
+    end
   end
 end
