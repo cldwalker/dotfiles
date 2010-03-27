de6c2f29c8bec73070182afb02fc4001979758cb
Gabriel Horner <gabriel.horner@gmail.com>
2010-03-27 16:52:08 -0400

    tweaked generators

--- .lightning/generators/other.rb
+++ .lightning/generators/other.rb
@@ -1,15 +1,17 @@
-module Lightning
-  module Generators
-    def bin
-      {:paths=>ENV['PATH'].split(":").uniq.map {|e| "#{e}/*" } }
-    end
+module Lightning::Generators
+  # td: remove subdirectories included in other directories
+  def ruby_load_path
+    {:paths=>$LOAD_PATH.select {|e| e[/^\//]}.uniq.map {|e| "#{e}/**/*.rb" } }
+  end
 
-    def ruby_load_path
-      {:paths=>$LOAD_PATH.select {|e| e[/^\//]}.uniq.map {|e| "#{e}/**/*.rb" } }
-    end
+  def pics_dir
+    {:paths=>["/Users/bozo/misc/pics/**/"]}
+  end
 
-    def pics_dir
-      {:paths=>["/Users/bozo/misc/pics/**/"]}
-    end
+  def wild_dir
+    {
+      :paths=>["**/"],
+      :desc=>"*ALL* directories under the current directory. Careful where you do this."
+    }
   end
 end
