34f5f2520bb55b2255e163e7a1d1a18e592ef1d2
Gabriel Horner <gabriel.horner@gmail.com>
2010-06-10 01:45:30 -0400

    getting rid of tabtab

--- .tabtab_definitions/gem.rb
+++
@@ -1,9 +0,0 @@
-TabTab::Definition.register('gem') do |c|
-  [:build, :cert, :check, :cleanup, :contents, :dependency, :environment, 
-    :fetch, :generate_index, :help, :install, :list, :lock, :mirror, :outdated,
-    :pristine, :query, :rdoc, :search, :server, :sources, :specification, :stale,
-    :uninstall, :unpack, :update, :which].each do |e|
-    c.command e
-  end
-end
-
