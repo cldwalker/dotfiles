79c152ea08a1f1b27e591e550678ceae5ecea7d5
Gabriel Horner <gabriel.horner@gmail.com>
2012-04-15 18:05:41 -0400

    fix gem bolt for 1.9 and rename

--- .lightning/generators/other.rb
+++ .lightning/generators/other.rb
@@ -7,6 +7,13 @@ module Lightning::Generators
     $LOAD_PATH.map {|e| "#{e}/**/*.rb" }
   end
 
+  def gem_new
+    arr = ["#{ENV['GEM_HOME']}/gems/*", "#{ENV['GEM_HOME']}/bundler/gems/*",
+     "vendor/ruby/*/{gems,bundler/gems}/*"]
+    require 'rubygems' unless defined? Gem
+    arr << "#{Gem.default_dir}/gems/*"
+  end
+
   def gem_path
     (ENV['GEM_PATH'] || '').split(':').map {|e| "#{e}/gems/*" }
   end
