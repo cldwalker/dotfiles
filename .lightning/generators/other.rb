4fd065eb8a2ea3057df0ac371febffec6d6ee1f2
Gabriel Horner <gabriel.horner@gmail.com>
2015-05-12 17:00:13 -0400

    Remove unused ruby bolts

--- .lightning/generators/other.rb
+++ .lightning/generators/other.rb
@@ -3,21 +3,6 @@ module Lightning::Generators
     $LOAD_PATH
   end
 
-  def ruby_load_path
-    $LOAD_PATH.map {|e| "#{e}/**/*.rb" }
-  end
-
-  def gem_new
-    arr = ["#{ENV['GEM_HOME']}/gems/*", "#{ENV['GEM_HOME']}/bundler/gems/*",
-     "{vendor,bundle}/ruby/*/{gems,bundler/gems}/*"]
-    require 'rubygems' unless defined? Gem
-    arr << "#{Gem.default_dir}/gems/*"
-  end
-
-  def gem_path
-    (ENV['GEM_PATH'] || '').split(':').map {|e| "#{e}/gems/*" }
-  end
-
   # td: remove subdirectories included in other directories
   desc "Ruby files in $LOAD_PATH"
   def ruby_load_path
@@ -32,12 +17,5 @@ module Lightning::Generators
   desc "System ruby directories"
   def ruby_dir
     system_ruby.map {|e| e +"/**/"}
-  end
-
-  # meta :commands=>[ {'shell_command'=>'open', 'desc'=>"open a gem's documentation in a browser",
-  #   'post_path'=>'/rdoc/index.html', 'name'=>'gem-doc' } ] }
-  desc "Gem documention directories"
-  def gem_doc
-    `gem environment path`.chomp.split(":").map {|e| e +"/doc/*" }
   end
 end
