afe8640340b8de3a78413a0d2ea0a5bda69347d1
Gabriel Horner <gabriel.horner@gmail.com>
2026-05-19 10:20:54 -0400

    switch fn to zsh to handle special chars

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -2,3 +2,10 @@
 zman() {
   PAGER="less -g -s '+/^       "$1"'" man zshall
 }
+
+# gll <WORD> [extract git log args] - invoke git log with search enabled in pager
+# Only use for seeing full commits as turning off -u leads to issues with LESS
+gll() {
+  local pat="+/$1" # Handles quoting for " and >
+  git -c core.pager="less ${(q)pat}" log -G "$1" -u "${@:2}"
+}
