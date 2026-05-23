a18db2b01a11d07afee1cba4563be71dc85c9fac
Gabriel Horner <gabriel.horner@gmail.com>
2026-05-23 09:39:21 -0400

    add a handy ns aliases fn which may be handy for a bit

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -5,7 +5,15 @@ zman() {
 
 # gll <WORD> [extract git log args] - invoke git log with search enabled in pager
 # Only use for seeing full commits as turning off -u leads to issues with LESS
-gll() {
+git-log-less() {
   local pat="+/$1" # Handles quoting for " and >
   git -c core.pager="less ${(q)pat}" log -G "$1" -u "${@:2}"
+}
+alias gll=git-log-less
+
+# clj-ns-aliases <NS> - Prints count of different aliases for a clj ns
+clj-ns-aliases() {
+  grep -rhE "\[$1 +:as +[a-zA-Z._?!*+<>-]+" --include='*.clj' --include='*.cljc' . \
+  | sed -E 's/.*:as +([a-zA-Z._?!*+<>-]+).*/\1/' \
+  | sort | uniq -c | sort -rn
 }
